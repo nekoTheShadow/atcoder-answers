271b261e2b97bfc8bebc83460ad1e3fe66ec2088
aa365296 <h.nakamura0903@gmail.com>
2020-04-03 19:25:45 +0900

    AtCoder Beginner Contest 029; D; Difficulty 1200-1599

---
+++ ABC029/d/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+const INFINITY = math.MaxInt64/2 - 1
+
+func exec(stdin *Stdin, stdout *Stdout) {
+	n := stdin.Read()
+	m, _ := strconv.Atoi(n)
+
+	ans := 0
+	for i := 0; i < len(n); i++ {
+		x := len(n) - i - 1
+		if n[x] == '0' {
+			ans += m / Pow(10, i+1) * Pow(10, i)
+		} else if n[x] == '1' {
+			ans += m / Pow(10, i+1) * Pow(10, i)
+			d, _ := strconv.Atoi(n[x+1:])
+			ans += d + 1
+		} else {
+			ans += (m/Pow(10, i+1) + 1) * Pow(10, i)
+		}
+	}
+	stdout.Println(ans)
+}
+
+func main() {
+	stdout := NewStdout()
+	defer stdout.Flush()
+	exec(NewStdin(bufio.ScanWords), stdout)
+}
+
+type Stdin struct {
+	stdin *bufio.Scanner
+}
+
+func NewStdin(split bufio.SplitFunc) *Stdin {
+	s := Stdin{bufio.NewScanner(os.Stdin)}
+	s.stdin.Split(split)
+	s.stdin.Buffer(make([]byte, bufio.MaxScanTokenSize), INFINITY)
+	return &s
+}
+
+func (s *Stdin) Read() string {
+	s.stdin.Scan()
+	return s.stdin.Text()
+}
+
+func (s *Stdin) ReadInt() int {
+	n, _ := strconv.Atoi(s.Read())
+	return n
+}
+
+func (s *Stdin) ReadFloat64() float64 {
+	n, _ := strconv.ParseFloat(s.Read(), 64)
+	return n
+}
+
+type Stdout struct {
+	stdout *bufio.Writer
+}
+
+func NewStdout() *Stdout {
+	return &Stdout{bufio.NewWriter(os.Stdout)}
+}
+
+func (s *Stdout) Flush() {
+	s.stdout.Flush()
+}
+
+func (s *Stdout) Println(a ...interface{}) {
+	fmt.Fprintln(s.stdout, a...)
+}
+
+func Min(a int, b ...int) int {
+	for _, v := range b {
+		if v < a {
+			a = v
+		}
+	}
+	return a
+}
+
+func Max(a int, b ...int) int {
+	for _, v := range b {
+		if a < v {
+			a = v
+		}
+	}
+	return a
+}
+
+func Abs(x int) int {
+	if x > 0 {
+		return x
+	} else {
+		return x * -1
+	}
+}
+
+func Pow(x, y int) int {
+	z := 1
+	for y > 0 {
+		if y%2 == 0 {
+			x *= x
+			y /= 2
+		} else {
+			z *= x
+			y -= 1
+		}
+	}
+	return z
+}
+
+func CreateMatrix(x, y int) [][]int {
+	matrix := make([][]int, x)
+	for i := 0; i < x; i++ {
+		matrix[i] = make([]int, y)
+	}
+	return matrix
+}
