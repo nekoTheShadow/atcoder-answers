24aef2a876ca0b187c8e2cfc11ea5c996ea022c3
hajime nakamura <h.nakamura0903@gmail.com>
2021-03-22 16:52:34 +0900

    AtCoder Beginner Contest 188; A-E; ~1600

---
+++ ABC188/D/Main.java
@@ -0,0 +1,215 @@
+package ABC188.D;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Main {
+    public void exec() {
+        int N = stdin.nextInt();
+        int C = stdin.nextInt();
+
+        Map<Integer, Long> d = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            int a = stdin.nextInt() - 1;
+            int b = stdin.nextInt() - 1;
+            long c = stdin.nextLong();
+            d.put(a, d.getOrDefault(a, 0L) + c);
+            d.put(b+1, d.getOrDefault(b+1, 0L) - c);
+        }
+
+        List<Integer> events = d.keySet().stream().sorted().collect(Collectors.toList());
+        long ans = 0;
+        long fee = 0;
+        long last = 0;
+        for (int event : events) {
+            if (last != event) {
+                ans += Math.min(C, fee) * (event-last);
+                last = event;
+            }
+            fee += d.get(event);
+        }
+        stdout.println(ans);
+    }
+
+
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+    private static final Stderr stderr = new Stderr(System.err, false);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    // ASCII ONLY
+    public static class Stdin {
+        private InputStream in;
+        private byte[] buf;
+        private int ptr;
+        private int len;
+
+        public Stdin(InputStream in) {
+            this.in = in;
+            this.buf = new byte[1024];
+            this.ptr = 0;
+            this.len = 0;
+        }
+
+        public String nextString() {
+            StringBuilder sb = new StringBuilder();
+            byte b;
+            while ((b = read()) != -1) {
+                sb.appendCodePoint(b);
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() {
+            return (int)nextLong();
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            boolean negative = false;
+            int x = 0;
+
+            byte b = read();
+            if (b == '-') {
+                negative = true;
+            } else {
+                x += b-'0';
+            }
+
+            while ((b=read()) != -1) {
+                x *= 10;
+                x += b-'0';
+            }
+
+            return negative ? -x : x;
+        }
+
+        private byte read() {
+            byte b = readByte();
+            if (b == '\r') {
+                readByte(); // LFを読み飛ばす
+                return -1;
+            } else if (b == '\n' || b == ' ') {
+                return -1;
+            } else {
+                return b;
+            }
+        }
+
+        private byte readByte(){
+            if (len == ptr) {
+                try {
+                    ptr = 0;
+                    len = in.read(buf);
+                    if (len == -1) return -1;
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return buf[ptr++];
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+
+    public static class Stderr {
+        private PrintWriter stderr;
+        private boolean debug;
+
+        public Stderr(PrintStream stderr, boolean debug) {
+            this.stderr =  new PrintWriter(stderr, false);
+            this.debug = debug;
+        }
+
+        public void println(Object ... objs) {
+            if (!debug) return ;
+
+            stderr.print("DEBUG: ");
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stderr.print(deepToString(objs[i]));
+                if (i != len-1) stderr.print(' ');
+            }
+            stderr.println();
+            stderr.flush();
+        }
+
+        private String deepToString(Object o) {
+            if (o == null) {
+                return "null";
+            }
+
+            // 配列の場合
+            if (o.getClass().isArray()) {
+                int len = Array.getLength(o);
+                String[] tokens = new String[len];
+                for (int i = 0; i < len; i++) {
+                    tokens[i] = deepToString(Array.get(o, i));
+                }
+                return "{" + String.join(", ", tokens) + "}";
+            }
+
+            return Objects.toString(o);
+        }
+    }
+}
