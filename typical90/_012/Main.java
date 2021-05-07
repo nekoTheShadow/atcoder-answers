2b8f6236218ec0543281f8aed5730f40cc1a73b0
hajime nakamura <h.nakamura0903@gmail.com>
2021-05-07 10:12:30 +0900

    競プロ典型 90 問: 012 - Red Painting（★4）

---
+++ typical90/_012/Main.java
@@ -0,0 +1,235 @@
+package typical90._012;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class Main {
+    public void exec() {
+        int h = stdin.nextInt();
+        int w = stdin.nextInt();
+        int q = stdin.nextInt();
+        
+        boolean[][] red = new boolean[h][w];
+        UnionFind uf = new UnionFind(h*w+1);
+        int[] dx = new int[]{1, -1, 0,  0};
+        int[] dy = new int[]{0,  0, 1, -1};
+        
+        while (q-- > 0) {
+            int query = stdin.nextInt();
+            if (query == 1) {
+                int r1 = stdin.nextInt()-1;
+                int c1 = stdin.nextInt()-1;
+                red[r1][c1] = true;
+                for (int i = 0; i < 4; i++) {
+                    int r2 = r1 + dx[i];
+                    int c2 = c1 + dy[i];
+                    if (0<=r2&&r2<h && 0<=c2&&c2<w && red[r2][c2]) {
+                        int h1 = r1*w+c1;
+                        int h2 = r2*w+c2;
+                        uf.union(h1, h2);
+                    }
+                }
+            } else {
+                int r1 = stdin.nextInt()-1;
+                int c1 = stdin.nextInt()-1;
+                int r2 = stdin.nextInt()-1;
+                int c2 = stdin.nextInt()-1;
+                if (red[r1][c1] && red[r2][c2]) {
+                    int h1 = r1*w+c1;
+                    int h2 = r2*w+c2;
+                    stdout.println(uf.same(h1, h2) ? "Yes" : "No");                	
+                } else {
+                    stdout.println("No");
+                }
+            }
+        }
+    }
+    
+    public class UnionFind {
+        private int[] parent;
+        private int[] size;
+
+        public UnionFind(int n) {
+            this.parent = IntStream.range(0, n).toArray();
+            this.size = new int[n];
+            Arrays.fill(this.size, 1);
+        }
+
+        public int find(int x) {
+            if (parent[x] == x) {
+                return x;
+            }
+            parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        public boolean same(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        public void union(int x, int y) {
+            x = find(x);
+            y = find(y);
+            if (x == y) {
+                return ;
+            }
+
+            if (size[x] < size[y]) {
+                parent[x] = y;
+                size[y] += size[x];
+            } else {
+                parent[y] = x;
+                size[x] += size[y];
+            }
+
+        }
+
+        public int size(int x) {
+            return size[find(x)];
+        }
+    }
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private Deque<String> queue;
+        private BufferedReader in;
+        private Pattern space;
+
+        public Stdin(InputStream in) {
+            this.queue = new ArrayDeque<>();
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.space = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            if (queue.isEmpty()) {
+                try {
+                    String line = in.readLine();
+                    if (line == null) {
+                        throw new EOFException();
+                    }
+                    space.splitAsStream(line).forEach(this.queue::addLast);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return queue.removeFirst();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+        
+        public BigInteger nextBigInteger() {
+            return new BigInteger(nextString());
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+        
+        public BigInteger[] nexBigIntegerArray(int n) {
+            BigInteger[] a = new BigInteger[n];
+            for (int i = 0; i < n; i++) a[i] = nextBigInteger();
+            return a;
+        }
+        
+        public List<Integer> nextIntegerList(int n) {
+            return nextList(n, this::nextInt);
+        }
+        
+        public List<Long> nextLongList(int n) {
+            return nextList(n, this::nextLong);
+        }
+        
+        public List<Double> nextDoubleList(int n) {
+            return nextList(n, this::nextDouble);
+        }
+        
+        public List<String> nextStringList(int n) {
+            return nextList(n, this::nextString);
+        }
+        
+        public List<BigInteger> nextBigIntegerList(int n) {
+            return nextList(n, this::nextBigInteger);
+        }
+        
+        private <T> List<T> nextList(int n, Supplier<T> supplier) {
+            List<T> a = new ArrayList<>();
+            for (int i = 0; i < n; i++) a.add(supplier.get());
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
