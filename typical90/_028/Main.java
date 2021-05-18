29b773aa2ebebd52f192bed9ae074e0b74f70cc6
hajime nakamura <h.nakamura0903@gmail.com>
2021-05-18 12:25:20 +0900

    競プロ典型 90 問: 028 - Cluttered Paper（★4）

---
+++ typical90/_028/Main.java
@@ -0,0 +1,201 @@
+package typical90._028;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public void exec() {
+        Imos2 imos2 = new Imos2(1000, 1000);
+        
+        int n = stdin.nextInt();
+        for (int i = 0; i < n; i++) {
+            int lx = stdin.nextInt();
+            int ly = stdin.nextInt();
+            int rx = stdin.nextInt();
+            int ry = stdin.nextInt();
+            imos2.add(lx, ly, rx, ry);
+        }
+        
+        long[][] result = imos2.run();
+        long[] ans = new long[n+1];
+        for (int x = 0; x <= 1000; x++) {
+            for (int y = 0; y <= 1000; y++) {
+                ans[(int)result[x][y]]++;
+            }
+        }
+        for (int i = 1; i <= n; i++) stdout.println(ans[i]);
+    }
+    
+    public class Imos2 {
+        private long[][] imos;
+        
+        public Imos2(int h, int w) {
+            this.imos = new long[h+1][w+1];
+        }
+        
+        public void add(int lx, int ly, int rx, int ry) {
+            imos[lx][ly]++;
+            imos[lx][ry]--;
+            imos[rx][ly]--;
+            imos[rx][ry]++;
+        }
+        
+        public long[][] run() {
+            for (int y = 0; y <= 1000; y++) {
+                for (int x = 1; x <= 1000; x++) {
+                    imos[y][x] += imos[y][x - 1];
+                }
+            }
+            
+            for (int y = 1; y <= 1000; y++) {
+                for (int x = 0; x <= 1000; x++) {
+                    imos[y][x] += imos[y - 1][x];
+                }
+            }
+            return imos;
+        }
+    }
+    
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private Deque<String> queue;
+        private BufferedReader in;
+        private Pattern space;
+
+        public Stdin(InputStream in) {
+            this.queue = new ArrayDeque<>();
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.space = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            if (queue.isEmpty()) {
+                try {
+                    String line = in.readLine();
+                    if (line == null) {
+                        throw new EOFException();
+                    }
+                    space.splitAsStream(line).forEach(this.queue::addLast);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return queue.removeFirst();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+        
+        public BigInteger nextBigInteger() {
+            return new BigInteger(nextString());
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+        
+        public BigInteger[] nexBigIntegerArray(int n) {
+            BigInteger[] a = new BigInteger[n];
+            for (int i = 0; i < n; i++) a[i] = nextBigInteger();
+            return a;
+        }
+        
+        public List<Integer> nextIntegerList(int n) {
+            return nextList(n, this::nextInt);
+        }
+        
+        public List<Long> nextLongList(int n) {
+            return nextList(n, this::nextLong);
+        }
+        
+        public List<Double> nextDoubleList(int n) {
+            return nextList(n, this::nextDouble);
+        }
+        
+        public List<String> nextStringList(int n) {
+            return nextList(n, this::nextString);
+        }
+        
+        public List<BigInteger> nextBigIntegerList(int n) {
+            return nextList(n, this::nextBigInteger);
+        }
+        
+        private <T> List<T> nextList(int n, Supplier<T> supplier) {
+            List<T> a = new ArrayList<>();
+            for (int i = 0; i < n; i++) a.add(supplier.get());
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
