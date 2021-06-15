db3b89defaf516b60e53ab79d98b988adeaa3190
hajime nakamura <h.nakamura0903@gmail.com>
2021-06-15 12:16:03 +0900

    競プロ典型 90 問: 060 - Chimera（★5）

---
+++ typical90/_060/Main.java
@@ -0,0 +1,200 @@
+package typical90._060;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+public class Main {
+    public void exec() {
+        int n = stdin.nextInt();
+        long[] a = stdin.nextLongArray(n);
+       
+        long[] c1 = lis(a);
+        reverse(a);
+        long[] c2 = lis(a);
+        reverse(c2);
+        
+        long ans = IntStream.range(0, n).mapToLong(i -> c1[i]+c2[i]-1).max().getAsLong();
+        stdout.println(ans);
+    }
+    
+    public long[] lis(long[] a) {
+        int n = a.length;
+        List<Long> b = new ArrayList<>();
+        long[] c = new long[n];
+        for (int i = 0; i < n; i++) {
+            int x = bisectLeft(b, a[i]);
+            if (b.size() == x) {
+                b.add(a[i]);
+            } else {
+                b.set(x, a[i]);
+            }
+            c[i] = x+1;
+        }
+        return c;
+    }
+    
+    public <T> int bisectLeft(List<? extends Comparable<? super T>> a, T x) {
+        int ng = -1;
+        int ok = a.size();
+        while (Math.abs(ok-ng) > 1) {
+            int mi = (ok+ng)/2;
+            if (a.get(mi).compareTo(x) >= 0) {
+                ok = mi;
+            } else {
+                ng = mi;
+            }
+        }
+        return ok;
+    }
+    
+    public void reverse(long[] a) {
+        for (int i = 0, n = a.length; i < n/2; i++) {
+            long t = a[i];
+            a[i] = a[n-i-1];
+            a[n-i-1] = t;
+        }
+    }
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private Deque<String> queue;
+        private BufferedReader in;
+        private Pattern space;
+
+        public Stdin(InputStream in) {
+            this.queue = new ArrayDeque<>();
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.space = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            if (queue.isEmpty()) {
+                try {
+                    String line = in.readLine();
+                    if (line == null) {
+                        throw new EOFException();
+                    }
+                    space.splitAsStream(line).forEach(this.queue::addLast);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return queue.removeFirst();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+        
+        public BigInteger nextBigInteger() {
+            return new BigInteger(nextString());
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+        
+        public BigInteger[] nexBigIntegerArray(int n) {
+            BigInteger[] a = new BigInteger[n];
+            for (int i = 0; i < n; i++) a[i] = nextBigInteger();
+            return a;
+        }
+        
+        public List<Integer> nextIntegerList(int n) {
+            return nextList(n, this::nextInt);
+        }
+        
+        public List<Long> nextLongList(int n) {
+            return nextList(n, this::nextLong);
+        }
+        
+        public List<Double> nextDoubleList(int n) {
+            return nextList(n, this::nextDouble);
+        }
+        
+        public List<String> nextStringList(int n) {
+            return nextList(n, this::nextString);
+        }
+        
+        public List<BigInteger> nextBigIntegerList(int n) {
+            return nextList(n, this::nextBigInteger);
+        }
+        
+        private <T> List<T> nextList(int n, Supplier<T> supplier) {
+            List<T> a = new ArrayList<>();
+            for (int i = 0; i < n; i++) a.add(supplier.get());
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
