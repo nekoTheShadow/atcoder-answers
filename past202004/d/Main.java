7095f432694a8eb975d0a2e898450efe3dd2af96
hajime nakamura <h.nakamura0903@gmail.com>
2021-07-23 21:56:09 +0900

    第二回 アルゴリズム実技検定 D - パターンマッチ

---
+++ past202004/d/Main.java
@@ -0,0 +1,120 @@
+package past202004.d;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class Main {
+    public void exec() {
+        String s = stdin.nextString();
+
+        
+        Set<String> d = new HashSet<>();
+        for (int len = 1; len <= 3; len++) {
+            for (int i = 0; i < s.length()-len+1; i++) {
+                d.addAll(f(s.substring(i, i+len)));
+            }
+        }
+
+        stdout.println(d.size());
+    }
+    
+    public List<String> f(String v) {
+        List<String> a = new ArrayList<>();
+        int n = v.length();
+        for (int bit = 0; bit < (1 << n); bit++) {
+            StringBuilder w = new StringBuilder(v);
+            for (int i = 0; i < n; i++) {
+                if ((bit & (1<<i)) != 0) w.setCharAt(i, '.');
+            }
+            a.add(w.toString());
+        }
+        return a;
+    }
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private Deque<String> queue;
+        private BufferedReader in;
+        private Pattern space;
+
+        public Stdin(InputStream in) {
+            this.queue = new ArrayDeque<>();
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.space = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            if (queue.isEmpty()) {
+                try {
+                    String line = in.readLine();
+                    if (line == null) {
+                        throw new EOFException();
+                    }
+                    space.splitAsStream(line).forEach(this.queue::addLast);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return queue.removeFirst();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+        
+        public BigInteger nextBigInteger() {
+            return new BigInteger(nextString());
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
