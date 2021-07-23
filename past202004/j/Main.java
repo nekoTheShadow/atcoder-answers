d6ddbcdc55c9711bf2a75880e85296172aadb182
hajime nakamura <h.nakamura0903@gmail.com>
2021-07-23 21:56:36 +0900

    第二回 アルゴリズム実技検定 J - 文字列解析

---
+++ past202004/j/Main.java
@@ -0,0 +1,122 @@
+package past202004.j;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.regex.Pattern;
+
+public class Main {
+    public void exec() {
+        String s = stdin.nextString();
+        
+        Deque<String> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            String c = s.substring(i, i+1);
+            if (c.equals(")")) {
+                StringBuilder sb = new StringBuilder();
+                while (true) {
+                    String d = stack.removeLast();
+                    if (d.equals("(")) {
+                        String q = sb.toString();
+                        sb.reverse();
+                        String p = sb.toString();
+                        stack.add(p+q);
+                        break;
+                    } else {
+                        sb.append(d);
+                    }
+                }
+            } else {
+                stack.addLast(c);
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.removeFirst());
+        }
+        stdout.println(sb.toString());
+        
+    }
+    
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private Deque<String> queue;
+        private BufferedReader in;
+        private Pattern space;
+
+        public Stdin(InputStream in) {
+            this.queue = new ArrayDeque<>();
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.space = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            if (queue.isEmpty()) {
+                try {
+                    String line = in.readLine();
+                    if (line == null) {
+                        throw new EOFException();
+                    }
+                    space.splitAsStream(line).forEach(this.queue::addLast);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return queue.removeFirst();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+        
+        public BigInteger nextBigInteger() {
+            return new BigInteger(nextString());
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
