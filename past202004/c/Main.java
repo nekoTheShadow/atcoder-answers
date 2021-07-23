abea3d85934c8f2e0aec59bc2a54ab61129495c8
hajime nakamura <h.nakamura0903@gmail.com>
2021-07-23 21:56:05 +0900

    第二回 アルゴリズム実技検定 C - 山崩し

---
+++ past202004/c/Main.java
@@ -0,0 +1,109 @@
+package past202004.c;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.regex.Pattern;
+
+public class Main {
+    public void exec() {
+        int n = stdin.nextInt();
+        char[][] s = new char[n][];
+        for (int i = 0; i < n; i++) s[i] = stdin.nextString().toCharArray();
+        
+        for (int i = n-2; i >=0; i--) {
+            for (int j = 0; j < 2*n-1; j++) {
+                if (s[i][j]=='#') {
+                    int c = 0;
+                    if (s[i+1][j-1]=='X') c++;
+                    if (s[i+1][j]=='X') c++;
+                    if (s[i+1][j+1]=='X') c++;
+                    if (c>0) s[i][j] = 'X';
+                }
+            }
+        }
+        
+        for (char[] row : s) stdout.println(new String(row));
+    }
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private Deque<String> queue;
+        private BufferedReader in;
+        private Pattern space;
+
+        public Stdin(InputStream in) {
+            this.queue = new ArrayDeque<>();
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.space = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            if (queue.isEmpty()) {
+                try {
+                    String line = in.readLine();
+                    if (line == null) {
+                        throw new EOFException();
+                    }
+                    space.splitAsStream(line).forEach(this.queue::addLast);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return queue.removeFirst();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+        
+        public BigInteger nextBigInteger() {
+            return new BigInteger(nextString());
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
