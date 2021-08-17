9072d9ce00aa899cf23657d02e5154bb2d0a81bb
neko_the_shadow <h.nakamura0903@gmail.com>
2021-08-17 10:47:12 +0900

    AtCoder Regular Contest 106; A-C; ~1600

---
+++ ARC106/A/Main.java
@@ -0,0 +1,124 @@
+package ARC106.A;
+
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.regex.Pattern;
+
+public class Main {
+    public void exec() {
+        long n = stdin.nextLong();
+        
+        long x = 1;
+        for (int a = 1; ; a++) {
+            x *= 3;
+            if (n < x) {
+                stdout.println(-1);
+                return ;
+            }
+            
+            long b = f(n-x);
+            if (b!=-1) {
+                stdout.println(a, b);
+                return ;
+            }
+        }
+    }
+    
+    public long f(long x) {
+        if (x==1) return -1;
+        
+        int p = 0;
+        while (x%5==0) {
+            p++;
+            x/=5;
+        }
+        if (x==1) {
+            return p;
+        } else {
+            return -1;
+        }
+    }
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private Deque<String> queue;
+        private BufferedReader in;
+        private Pattern space;
+
+        public Stdin(InputStream in) {
+            this.queue = new ArrayDeque<>();
+            this.in = new BufferedReader(new InputStreamReader(in));
+            this.space = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            if (queue.isEmpty()) {
+                try {
+                    String line = in.readLine();
+                    if (line == null) {
+                        throw new EOFException();
+                    }
+                    space.splitAsStream(line).forEach(this.queue::addLast);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return queue.removeFirst();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+        
+        public BigInteger nextBigInteger() {
+            return new BigInteger(nextString());
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
